d28084ce73913d4c15e5316e28cdf78c19e87984
rahn <ryanahn2000@gmail.com>
2021-08-02 09:06:17 -0700

    Initial Commit

---
+++ Calendar Assignments/Calendar_AssignmentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calendar_AssignmentsApp.swift
+//  Calendar Assignments
+//
+//  Created by Ryan Ahn on 8/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Calendar_AssignmentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calendar AssignmentsTests/Calendar_AssignmentsTests.swift
@@ -0,0 +1,33 @@
+//
+//  Calendar_AssignmentsTests.swift
+//  Calendar AssignmentsTests
+//
+//  Created by Ryan Ahn on 8/2/21.
+//
+
+import XCTest
+@testable import Calendar_Assignments
+
+class Calendar_AssignmentsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
